6f1693dc2b30b020b6d3129632eb3f3607aaf481
R4D3N G0Z4LL <radengozal@gmail.com>
2025-08-20 09:39:17 +0700

    feat: Enhance error response handling with detailed error information

--- utils/response.js
+++ utils/response.js
@@ -5,7 +5,23 @@ exports.responseSuccessWithMessage = (
   message: message,
 });
 exports.responseErrorWithMessage = (
-  message = "Upsss... Something went wrong on server"
-) => ({
-  message: message,
-});
+  message = "Upsss... Something went wrong on server",
+  error = null
+) => {
+  const response = {
+    message: message,
+  };
+  if (error) {
+    response.error = {
+      name: error.name,
+      code: error.code || error.status || undefined,
+      details: error.message,
+      stack: error.stack,
+    };
+
+    if (error.response && error.response.data) {
+      response.error.response = error.response.data;
+    }
+  }
+  return response;
+};
